530dd3281364d38239c0e72a6082c688848bbdcb
Ruslan <r.gusseinov@mail.ru>
2021-07-13 22:59:20 +0600

    Working on loader component and css modules

--- src/api/api.js
+++ src/api/api.js
@@ -3,30 +3,14 @@ const URL = 'https://v1.nocodeapi.com/ruslan/cx/RgorThEpEQxhOckt/rates';
 // https://v1.nocodeapi.com/ruslan/cx/RgorThEpEQxhOckt/rates?source=EUR
 // https://v1.nocodeapi.com/ruslan/cx/RgorThEpEQxhOckt/rates/convert?amount=100&from=KZT&to=RUB
 
-
 export async function getCurrentRateList(baseCurrency){
-
   const result = await fetch(`${URL}?source=${baseCurrency}`);
   if (!result.ok) return Promise.reject(`Что то пошло не так: ${result}`)
   return result.json();
-
 }
 
-/* export function getCurrentRateList(baseCurrency) {
-  try {
-    const data = fetch(`${URL}?source=${baseCurrency}`);
-    return data.then((result) => result.json());
-  } catch (err) {
-    throw new Error(`Что то пошло не так: ${err}`)
-  }
+export async function getConvertedCurrencyList(amount, baseCurrency, toCurrency) {
+  const result = await fetch(`${URL}/convert?amount=${amount}&from=${baseCurrency}&to=${toCurrency}`);
+  if (!result.ok) return Promise.reject(`Что то пошло не так: ${result}`);
+  return result.json();  
 }
- */
-export function getConvertedCurrencyList(amount, baseCurrency, toCurrency) {
-  try {
-    const data = fetch(`${URL}/convert?amount=${amount}&from=${baseCurrency}&to=${toCurrency}`);
-    return data.then((result) => result.json());
-  } catch (err) {
-    throw new Error(`Что то пошло не так: ${err}`)
-  }
-}
-
---
+++ src/components/Loader/loader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CircularProgress, Grid } from '@material-ui/core'
+import classes from './loader.module.css';
+
+const Loader: React.FC = () => {
+  return (
+    <div className={ classes.mt20 }>
+      <Grid container justify="center" alignItems="center">
+        <CircularProgress />
+      </Grid>
+    </div>
+  )
+}
+
+export default Loader
